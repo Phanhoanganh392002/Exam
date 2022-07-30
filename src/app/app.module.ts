c2326e6309d8d84966c74293c1b33b4371741af9
Phanhoanganh392002 <dfang99999@gmail.com>
2022-07-30 10:33:45 +0700

    baitap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule, Routes} from "@angular/router";
 import { AppComponent } from './app.component';
+import {ExamComponent} from "./eaxm/exam.component";
+const appRoutes: Routes = [
+  {path: '', component: ExamComponent},
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/eaxm/exam.component.ts
@@ -0,0 +1,9 @@
+import {Component, Input} from "@angular/core";
+
+@Component({
+  selector:'app-exam',
+  templateUrl:'./exam.component.html'
+})
+export class ExamComponent{
+
+}
